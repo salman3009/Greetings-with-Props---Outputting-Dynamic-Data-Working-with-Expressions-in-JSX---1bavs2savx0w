7386d92f5b74821155ef0c578cb804a41bd3efad
salman3009 <salman444g@gmail.com>
2022-10-28 20:03:46 +0530

    code

---
+++ solution/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component, useState } from "react";
+import '../styles/App.css';
+import Welcome from "./Welcome;";
+
+const App = () => {
+  return (
+    <Welcome name=""></Welcome>
+  )
+}
+
+
+export default App;
---
+++ solution/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function child(props) {
+
+    return (
+        <div>
+            <h1>Hey {props.name}!</h1>
+            <h2>Welcome to Newton School.</h2>
+        </div>
+    )
+}
